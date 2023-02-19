d15515953cd44f5afaa4f7ca78558e74b222cefb
juandi131 <juandigarcia3@gmail.com>
2023-02-19 15:51:17 -0800

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - This programe worcs
+ *
+ * Return: send 0
+ */
+
+int main(void)
+{
+	int n;
+	char l;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar('0' + n);
+	}
+	for (l + 'a'; l <= 'f'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
