32deca9fccecc75f1641ac6de2e82fc3d76fd00e
juandi131 <juandigarcia3@gmail.com>
2023-02-19 15:23:05 -0800

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This programe worcs
+ *
+ * Return: send 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
