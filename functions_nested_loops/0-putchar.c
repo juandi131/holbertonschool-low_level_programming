673f71fbb36bc024bd1f14cd37c0aaa0fe6791f3
juandi131 <juandigarcia3@gmail.com>
2023-02-19 17:10:35 -0800

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+	#include "main.h"
+/**
+ * main - Print the alphabet  in upercase.
+ *
+ * Return: Send when the programe succes
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
