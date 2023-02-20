2c3e40ee297a27ab1b98604bca8c540de90f7980
juandi131 <juandigarcia3@gmail.com>
2023-02-19 17:27:11 -0800

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Print the alphabet  in upercase.
+ *
+ * Return: Send when the programe succes
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
