bdb498d90adce63953c22d2f13924cde1dfed371
juandi131 <juandigarcia3@gmail.com>
2023-02-23 04:13:43 -0800

    0-isupper.c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,7 +4,6 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-void print_numbers(void)
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
