9878cdf098484c42177dd4e587e8912a5e0cbd5a
juandi131 <juandigarcia3@gmail.com>
2023-02-23 08:58:01 -0800

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,17 +8,12 @@
 void print_most_numbers(void)
 {
 	int i;
-	for (i = 0; i <= 9, i++}
-	{
-		if (i < 2 && i > 4)
-		{
-			_putchar(i)
-		}
-		else if (i >2 && i >4)
-	}
-	else 
-		return (0)
-}
 
-		
-			
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+}
