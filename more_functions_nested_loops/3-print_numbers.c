63bc88fb89b471644177e8df6144af4da7ea39ad
juandi131 <juandigarcia3@gmail.com>
2023-02-23 08:06:19 -0800

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *print_numbers- print numbers
+ *print_numbers
  *@c: variable
  *Return:dassadsasdad
  */
@@ -10,8 +10,7 @@ void print_numbers(void)
 
 	for (c = 0; c <= 9; c++)
 	{
-		_putchar(c);
+		_putchar(c + '0');
 	}
 	_putchar('\n');
-	return (0);
 }
