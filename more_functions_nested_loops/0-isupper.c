33f65ef764dd59b40d545e92ec04251a3e33886c
juandi131 <juandigarcia3@gmail.com>
2023-02-22 19:17:37 -0800

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _isdigit- me escape del mundo yedome al norte
+ *
+ * Return: La haces muy bien
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+
+		return (0);
+}
