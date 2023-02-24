cf77964b7ee5497a9a5c551c060ef94a10218b3a
juandi131 <juandigarcia3@gmail.com>
2023-02-24 11:57:11 -0800

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_line- Muchas gracias palermo
+ *@n: variable
+ *
+ */
+void print_line(int n)
+{
+	int i;
+	int p;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			p = 95;
+			_putchar(p);
+		}
+	_putchar('\n');
+	}
+}
