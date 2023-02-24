7a40240db0d06f9113107d457ad1c867dbf72c4e
juandi131 <juandigarcia3@gmail.com>
2023-02-24 15:39:34 -0800

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square- Muchas gracias Palermo
+ * @size: variable
+ */
+void print_square(int size)
+{
+	int i;
+	int d;
+	int w;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (d = 0; d < size; d++)
+			{
+				w = 35;
+				_putchar(w);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n')
+}
