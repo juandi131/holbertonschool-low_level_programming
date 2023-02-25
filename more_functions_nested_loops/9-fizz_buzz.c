27d2be52b8a38f15274f9a31283a628d1602ae5f
juandi131 <juandigarcia3@gmail.com>
2023-02-24 16:42:11 -0800

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main- Muchas gracias Palermo
+ * Return: agradecido con palermo
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
