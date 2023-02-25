df6defcf1809dca40a7be26133dd41ed90ae60da
juandi131 <juandigarcia3@gmail.com>
2023-02-24 18:11:12 -0800

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i == 100)
+		{
+			printf("Buzz \n");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -25,6 +29,5 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
-	printf("\n");
 	return (0);
 }
