de60a65095b575f9844231b5e9a8346bf07880e7
juandi131 <juandigarcia3@gmail.com>
2023-02-28 15:28:31 -0800

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts- Muchas gracias Palermo
+ * @str: Muchas gracias Palermo
+ *
+ */
+void _puts(char *str)
+{
+	char a;
+
+	for (a = 0; str[a]  != '\0'; a++)
+	       _putchar(a)
+}
