b5bc86f11ae688484a03b80359130f56359a5e45
juandi131 <juandigarcia3@gmail.com>
2023-02-28 17:38:26 -0800

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev- MUCHAS GRACIAS PALERMO!!!!
+ *@s: Lo que hiciste por boca no se olvida en la vida
+ *
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 50; s[i] != '0'; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
