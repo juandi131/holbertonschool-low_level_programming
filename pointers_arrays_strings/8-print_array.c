07b51faec74aab3cd55cdf8b737dda8d63d01e19
juandi131 <juandigarcia3@gmail.com>
2023-03-01 09:17:36 -0800

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_array- mUCHAS GRACIAS PALERMO!!
+ * @a: Lo que hiciste por boca no se olvida en la vida.
+ * @n: No se olvida en la vida
+ */
+void print_array(int *a, int n)
+{
+	int c;
+
+	for (c = 0; c < n; c++)
+	{
+		printf("%d ", c);
+	}
+	printf("\n");
+}
