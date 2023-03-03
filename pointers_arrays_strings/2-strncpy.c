9082a75415626daafd02af4d5ff8ce535717c4cf
juandi131 <juandigarcia3@gmail.com>
2023-03-03 05:16:36 -0800

    asasa

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ **_strncpy- dadadasdasda
+ *@n: adada
+ *@src: asdda
+ *@dest: sdadasd
+ *Return: asdadas
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i <= n && src[i] != 0)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
