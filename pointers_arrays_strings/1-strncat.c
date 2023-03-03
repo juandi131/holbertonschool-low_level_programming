9cb7b0297c35521f70627f9319d2890565f6c5d7
juandi131 <juandigarcia3@gmail.com>
2023-03-02 17:50:15 -0800

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strncat- hola
+ * @n: Muchas gracias Palermo
+ * @dest: dest
+ * @src: hola
+ * Return: skay
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int v;
+	int a;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		v = i;
+	}
+	a = 0;
+	while (a <= n)
+	{
+		dest[i] = src[a];
+		i++;
+		a++;
+	}
+	return (dest);
+}
