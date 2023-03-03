a7036c047a55a12f8c0310ab5de760c0e326ef49
juandi131 <juandigarcia3@gmail.com>
2023-03-03 05:48:35 -0800

    dedede

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,11 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-	while (i <= n)
+	while (i <= n && i != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0'
 	return (dest);
 }
