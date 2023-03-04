a0559e24246cfe34158799497b44df54f8b6dea3
juandi131 <juandigarcia3@gmail.com>
2023-03-04 10:56:54 -0800

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper- sadasdadsa
+ * @a: dsdadsaddsad
+ * Return: ssadsadad
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (a[i]  >= 'a' && a[i] <= 'z')
+		a[i] = a[i] - 32;
+		return (a);
+	}
+}
