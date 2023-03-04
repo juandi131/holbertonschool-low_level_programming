2d4bd7a33182395e3cbd2e585951db37b6112711
juandi131 <juandigarcia3@gmail.com>
2023-03-04 11:32:02 -0800

    sdasd

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,12 @@
 char *string_toupper(char *a)
 {
 	int i;
-	
+
 	i = 0;
-	while (i != '\0')
+	while (a[i] != '\0')
 	{
 		if (a[i]  >= 'a' && a[i] <= 'z')
-		{	
+		{
 			a[i] = a[i] - 32;
 		}
 	i++;
