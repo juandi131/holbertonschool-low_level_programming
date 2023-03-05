7adb7850c98ae748decec83a1f42669bc879be73
juandi131 <juandigarcia3@gmail.com>
2023-03-05 14:49:38 -0800

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,8 +11,8 @@ char *cap_string(char *a)
 	i = 0;
 	if (a[i] >= 'a' && a[i] <= 'z')
 	{
-	          a[i] =  a[i] - 32;
-	}	
+		a[i] = a[i] - 32;
+	}
 	for (i = 0; a[i] != '\0'; i++)
 	{
 		if ((a[i] == '\n') || (a[i] == '\t') || (a[i] == ' ') || (a[i] == ',') ||
