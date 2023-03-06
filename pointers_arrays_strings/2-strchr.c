8d0324b20b1261bbcaacd2938727de1e7a3b886b
juandi131 <juandigarcia3@gmail.com>
2023-03-05 17:52:41 -0800

    -

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- *
- *
- *
+ * *_strchr- -
+ * @s: -
+ * @c: -
+ * Return: -
  */
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
 {
-	int i
+	int i;
 
 	for (i = 0; i != '\0'; i++)
 	{
 		if (c == s[i])
 		{
 			return (s[i]);
-		}	
-		else if (s[i + 1] == '\0')
+		}
+		else if (s[(i + 1)] == '\0')
 		{
 			return ('\0');
 		}
