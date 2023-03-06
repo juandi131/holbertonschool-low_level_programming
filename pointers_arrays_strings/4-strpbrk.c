7e9302e15a4b6f7a70adeb57587bcc7d18ae4fad
juandi131 <juandigarcia3@gmail.com>
2023-03-05 18:49:43 -0800

    asd

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ **_strpbrk- - 
+ *@s: -
+ *@accept: -
+ *Return: -
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (s[a] = '\0')
+	{
+		for (b + 0; accpt[b] != '\0'; b++)
+			if (s[a] == accept[b])
+				return (&s[a]);
+		a++;
+	}
+	return ('\0');
+}
