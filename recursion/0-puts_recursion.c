0802074c1ce2e7524312239e76d412986c983eb2
juandi131 <juandigarcia3@gmail.com>
2023-03-08 09:25:19 -0800

    1-print_rev_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,9 +5,8 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s[0] == '\0')
+	if (s[0] == 0)
 	{
-		_putchar('\n');
 	}
 	else
 	{
--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
- * _prin``t_rev_recursion- -
+ * _print_rev_recursion- -
  * @s: -
  */
 void _print_rev_recursion(char *s)
 {
 	if (s[0] == 0)
 	{
-		_putchar('\n');
 	}
 	else
 	{
-		_print_rev_recursion(&s[1]);
+		_print_rev_recursion(*s[1]);
 		_putchar(s[1]);
-		_putchar('\n');
 	}
 
 }
