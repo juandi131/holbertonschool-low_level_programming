74f34e635e5ba9e44b1f769d86836f59b6530245
juandi131 <juandigarcia3@gmail.com>
2023-03-08 13:27:01 -0800

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,13 +5,14 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (s[0] == 0)
+	if (s[0] == '\0')
 	{
+	
 	}
 	else
 	{
 		_print_rev_recursion(&s[1]);
-		_putchar(s[1]);
+		_putchar(s[0]);
 	}
 
 }
