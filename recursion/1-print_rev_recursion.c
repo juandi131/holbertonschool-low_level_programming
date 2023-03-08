01751b7216aac4fbdbb5aa1533207c12a566fc40
juandi131 <juandigarcia3@gmail.com>
2023-03-08 09:06:35 -0800

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _puts_rev_recursion- -
+ * _print_rev_recursion- -
  * @s: -
  */
 void _print_rev_recursion(char *s)
 {
-	if (s[0] == 0)
+	if (s[0] == '\n')
 	{
 		_putchar('\n');
 	}
