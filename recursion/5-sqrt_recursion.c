0543138d0c6fad60e50c28d1e0f38415cf05cd5a
juandi131 <juandigarcia3@gmail.com>
2023-03-11 12:32:57 -0800

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _sqrt_recursion- Write a function that returns the natural square root of a number.
+ * @n: Variable
+ * Return: This an int.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	else 
+	{
+		return (creayrecursa(1, n));
+	}
+}
+/**
+ * creayrecursa- this functions creates a variable.
+ *@b:  This is a variable 
+ *@a: This is a variable.
+ *Return:  returns  a int 
+ */
+
+int creayrecursa(int b, int n)
+{	
+	if (b * b == n) 
+	{
+		return (b);
+	}
+	else if	(b < n)
+	{
+		return (creayrecursa(b + 1, n));
+	}
+	else
+	{
+		return (-1);
+
+	}
+}	
