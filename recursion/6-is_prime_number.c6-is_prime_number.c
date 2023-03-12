f013dcff05663892d399051df773cd9d87f2af2c
juandi131 <juandigarcia3@gmail.com>
2023-03-12 14:01:38 -0700

    6-is_prime_number.c6-is_prime_number.c

---
+++ recursion/6-is_prime_number.c6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ *is_prime_number- Find the primes
+ *@n: This is the imput
+ *Return: Returns 1 if is a prime.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (encuentra_los_primos(2, n));
+	}
+}
+/**
+ *encuentra_los_primeros- auxiliar function
+ *@b: This is a variable.
+ *@n: this is a variable
+ *Return: Returns a variable.
+ */
+int encuentra_los_primeros(int b, int n)
+{
+	if (n % b == 0)
+	{
+		return (0);
+	}
+	else if (b < n)
+	{
+		return (encuentra_los_primos(b + 1, n));
+	}
+	else
+	{
+		return (1);
+	}
+}
