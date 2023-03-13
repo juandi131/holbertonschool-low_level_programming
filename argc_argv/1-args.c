c3dc5ec5174ad16710867309b01fe3182bf9c4f4
juandi131 <juandigarcia3@gmail.com>
2023-03-12 19:51:26 -0700

    git

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- print the lenght 
+ * @argv: -
+ * @argc: -
+ * Retunr: -
+ */
+int main(int argc, char *argv)
+{
+	printf("%d", argc);
+	(void)argv;
+	return (0);
+}	
